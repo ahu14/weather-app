146680a2610b37622885652cf3aaf1e5863bd4fc
sherlyhu28 <sherlyhu28@gmail.com>
2023-03-30 17:16:51 +0700

    move link to https and trying to set custom header

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,39 @@
 import "./style/Home.css";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 
 let Home = () => {
     let [data, setData] = useState([]);
 
     let clicked = async (cityData, code) => {
-        return await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${code}?apikey=${process.env.REACT_APP_WEBSITE_API_KEY}&metric=true`)
+        return await fetch(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${code}?apikey=${process.env.REACT_APP_WEBSITE_API_KEY}&metric=true`)
         .then(res => res.json())
         .then(d => {
-            setData(prev => prev = [
-                <h2 key="title" className="mt-5 font-bold">{cityData.city}, {cityData.province}, {cityData.country}</h2>
-            ])
+            setData(prev => {
+                return prev = [<h2 key="title" className="mt-5 font-bold">{cityData.city}, {cityData.province}, {cityData.country}</h2>]
+            })
 
             d.DailyForecasts.map((d2, index) => {
                 if (data.length > 0){
                     setData(data = []);
                 }
 
-                setData(prev => prev.concat(
-                    <div className="mt-3" key={index}>
-                        <p>{d2.Date}</p>
-                        <p>Day Weather : {d2.Day.IconPhrase}</p>
-                        <p>Night Weather : {d2.Night.IconPhrase}</p>
-                        <p>{d2.Temperature.Minimum.Value} C - {d2.Temperature.Maximum.Value} C</p>
-                    </div>
-                ))
+                return setData(prev => {
+                    return prev.concat(
+                        <div className="mt-3" key={index}>
+                            <p>{d2.Date}</p>
+                            <p>Day Weather : {d2.Day.IconPhrase}</p>
+                            <p>Night Weather : {d2.Night.IconPhrase}</p>
+                            <p>{d2.Temperature.Minimum.Value} C - {d2.Temperature.Maximum.Value} C</p>
+                        </div>
+                    )
+                })
             })
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            setData(prev => prev = [err.message])
+            throw Error(err.message);
+        });
     }
 
     let submitted = async (event) => {
@@ -39,29 +44,34 @@ let Home = () => {
             setData(data = []);
         }
 
-        return await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${process.env.REACT_APP_WEBSITE_API_KEY}&q=${city}&offset=100`)
+        return await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${process.env.REACT_APP_WEBSITE_API_KEY}&q=${city}&offset=100`)
         .then(res => res.json())
         .then(d => {
-            if (d.length == 0){
+            if (d.length === 0){
                 setData(prev => prev.concat(<p key="haha">City Not Found</p>))
             }
 
             else{
                 d.map((d, index) => {
-                    setData(prev => prev.concat(
-                        <div key={index} className="font-normal hover:font-bold cursor-pointer" 
-                        onClick={() => clicked({
-                            city : d.EnglishName,
-                            province : d.AdministrativeArea.EnglishName,
-                            country : d.Country.EnglishName,
-                        }, d.Key)}>
-                            <p>{d.EnglishName}, {d.AdministrativeArea.EnglishName}, {d.Country.EnglishName}</p>
-                        </div>
-                    ));
+                    return setData(prev => {
+                        return prev.concat(
+                            <div key={index} className="font-normal hover:font-bold cursor-pointer" 
+                            onClick={() => clicked({
+                                city : d.EnglishName,
+                                province : d.AdministrativeArea.EnglishName,
+                                country : d.Country.EnglishName,
+                            }, d.Key)}>
+                                <p>{d.EnglishName}, {d.AdministrativeArea.EnglishName}, {d.Country.EnglishName}</p>
+                            </div>
+                        )
+                    })
                 })
             }
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            setData(prev => prev = [err.message])
+            throw Error(err.message);
+        });
     }
 
     return (
