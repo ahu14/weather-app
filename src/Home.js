f71e7f6589ec6636b48b7483a7c4c16717dd1595
sherlyhu28 <sherlyhu28@gmail.com>
2023-04-13 16:40:26 +0700

    change the way the api get information

--- src/Home.js
+++ src/Home.js
@@ -1,89 +1,95 @@
 import "./style/Home.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
+let Template = ({isOn, title, date, data}) => {
+    switch (isOn) {
+        case false:
+            return (
+                <div className="flex w-full h-screen justify-center items-center">
+                    <div className="w-24 h-24 border-8 rounded-full border-r-amber-200 
+                    border-t-amber-300 border-l-amber-400 border-b-amber-500 animate-spin"></div>
+                </div>
+            )
+
+        default :
+            return (
+                <>
+                    <div className="font-bold text-xl text-center mt-3">
+                        <h2>{title}</h2>
+                        <h2>{date}</h2>
+                    </div>
+
+                    <div className="p-2 grid w-full h-screen justify-center items-center text-center">
+                        <div className="w-fit h-full grid grid-flow-cols sm:grid-cols-3">
+                            {data}
+                        </div>
+                    </div>
+                </>
+            )
+    }
+}
 
 let Home = () => {
     let [data, setData] = useState([]);
-
-    let clicked = async (cityData, code) => {
-        return await fetch(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${code}?apikey=${process.env.REACT_APP_WEBSITE_API_KEY}&metric=true`)
-        .then(res => res.json())
-        .then(d => {
-            setData(prev => {
-                return prev = [<h2 key="title" className="mt-5 font-bold">{cityData.city}, {cityData.province}, {cityData.country}</h2>]
-            })
-
-            d.DailyForecasts.map((d2, index) => {
-                if (data.length > 0){
-                    setData(data = []);
-                }
-
-                return setData(prev => {
-                    return prev.concat(
-                        <div className="mt-3" key={index}>
-                            <p>{d2.Date}</p>
-                            <p>Day Weather : {d2.Day.IconPhrase}</p>
-                            <p>Night Weather : {d2.Night.IconPhrase}</p>
-                            <p>{d2.Temperature.Minimum.Value} C - {d2.Temperature.Maximum.Value} C</p>
-                        </div>
-                    )
-                })
-            })
-        })
-        .catch(err => {
-            setData(prev => prev = [err.message])
-            throw Error(err.message);
-        });
-    }
-
-    let submitted = async (event) => {
-        event.preventDefault();
-        let city = event.target.city.value;
-
-        if (data.length > 0){
-            setData(data = []);
+    let [date, setDate] = useState("");
+    let [title, setTitle] = useState("");
+    let [isOn, setIsOn] = useState(navigator.onLine);
+    
+    useEffect(() => {
+        let checkSignal = () => {
+            setIsOn(isOn = navigator.onLine);
+            requestAnimationFrame(checkSignal);
         }
 
-        return await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${process.env.REACT_APP_WEBSITE_API_KEY}&q=${city}&offset=100`)
-        .then(res => res.json())
-        .then(d => {
-            if (d.length === 0){
-                setData(prev => prev.concat(<p key="haha">City Not Found</p>))
-            }
+        requestAnimationFrame(checkSignal);
+    }, []);
 
-            else{
-                d.map((d, index) => {
-                    return setData(prev => {
-                        return prev.concat(
-                            <div key={index} className="font-normal hover:font-bold cursor-pointer" 
-                            onClick={() => clicked({
-                                city : d.EnglishName,
-                                province : d.AdministrativeArea.EnglishName,
-                                country : d.Country.EnglishName,
-                            }, d.Key)}>
-                                <p>{d.EnglishName}, {d.AdministrativeArea.EnglishName}, {d.Country.EnglishName}</p>
+
+    useEffect(() => {
+        let showPosition = (pos) => {
+            let longitude = pos.coords.longitude;
+            let latitude = pos.coords.latitude;
+
+            fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_API_KEY}&q=${latitude}%2C${longitude}`)
+            .then(res => res.json())
+            .then(d => {
+                if (title != "" || data.length != 0 || date != ""){
+                    setTitle(title = "");
+                    setData(data = []);
+                    setDate(date = "");
+                }
+
+                setTitle(
+                    title = `${d.LocalizedName}, ${d.ParentCity.LocalizedName}, 
+                    ${d.AdministrativeArea.LocalizedName}, ${d.Country.LocalizedName}`
+                );
+
+                fetch(`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${d.Key}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`)
+                .then(res => res.json())
+                .then(d2 => {
+                    for (let i = 0; i < d2.length; i++){
+                        let dateTimeInfo = d2[i].DateTime.split(/[T+]/);
+
+                        setDate(date = dateTimeInfo[0]);
+                        setData(prev => prev.concat([
+                            <div key={i} className="px-7 mt-3 p-3 text-l border-2 border-amber-300 rounded sm:mx-2 px-5 py-7">
+                                <h2>{dateTimeInfo[1]}</h2>
+                                <p>{d2[i].IconPhrase}</p>
+                                <p>{d2[i].Temperature.Value}&#176;{d2[i].Temperature.Unit}</p>
                             </div>
-                        )
-                    })
+                        ]))
+                    }
                 })
-            }
-        })
-        .catch(err => {
-            setData(prev => prev = [err.message])
-            throw Error(err.message);
-        });
-    }
+                .catch(err => console.log(err.message));
+            })
+            .catch(err => console.log(err.message));
+        }
 
-    return (
-        <div className="p-4">
-            <form onSubmit={submitted}>
-                <input id="city" className="border-2 p-1" />
-                <button className="block mt-3 mb-3 p-2 border-2">Search</button>
-            </form>
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }, [isOn == true]);
 
-            {data}
-        </div>
-    )
+
+    return <Template isOn={isOn} title={title} date={date} data={data} />
 }
 
 export default Home;
